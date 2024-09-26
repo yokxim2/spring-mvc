6d1637577bdd77da69500f67661dfeb388e7ba91
yokxim <yooksi53@gmail.com>
2024-09-26 13:56:18 +0900

    Controller understanding

--- src/main/java/com/sparta/springmvc/controller/HelloController.java
+++ src/main/java/com/sparta/springmvc/controller/HelloController.java
@@ -5,32 +5,33 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
+@RequestMapping("/api")
 public class HelloController {
-    @GetMapping("/api/hello")
+    @GetMapping("/hello")
     @ResponseBody
     public String hello() {
         return "hello World";
     }
 
-    @GetMapping("/api/get")
+    @GetMapping("/get")
     @ResponseBody
     public String get() {
         return "GET Method 요청";
     }
 
-    @PostMapping("/api/post")
+    @PostMapping("/post")
     @ResponseBody
     public String post() {
         return "POST Method 요청";
     }
 
-    @PutMapping("/api/put")
+    @PutMapping("/put")
     @ResponseBody
     public String put() {
         return "PUT Method 요청";
     }
 
-    @DeleteMapping("/api/delete")
+    @DeleteMapping("/delete")
     @ResponseBody
     public String delete() {
         return "DELETE Method 요청";
