700f3849a5736914bee45f9b89f63b1a7db1fc46
yokxim <yooksi53@gmail.com>
2024-09-26 16:00:05 +0900

    HTTP data to object

---
+++ src/main/java/com/sparta/springmvc/request/Star.java
@@ -0,0 +1,12 @@
+package com.sparta.springmvc.request;
+
+public class Star {
+
+    String name;
+    int age;
+
+    public Star(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
